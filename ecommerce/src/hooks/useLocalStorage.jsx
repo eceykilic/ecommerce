a5015ce58073083f38969e556b3b4cfff3ae8260
eceykilic <eceykilic@gmail.com>
2024-02-22 13:45:22 +0300

    local storage arrangements

--- ecommerce/src/hooks/useLocalStorage.jsx
+++ ecommerce/src/hooks/useLocalStorage.jsx
@@ -2,8 +2,12 @@ import { useState } from "react";
 
 const useLocalStorage = (key, initialValue) => {
     const [storedValue, setStoredValue] = useState(() => {
+        
+        // başlangıç değerini ayarladık useState ile:
         const item = localStorage.getItem(key);
         if (item) {
+
+            // json parse ile json formatına çevrilmeye çalışılır (try) dönüşemezse initial dönüşürse bu veri kullanılır.
             try {
                 return JSON.parse(item);
             } catch (e) {
@@ -12,14 +16,18 @@ const useLocalStorage = (key, initialValue) => {
         } else return initialValue;
     });
     const setValue = (value) => {
+        //soredValue güncellenir ve güncellenmiş değer localde kaydedilir.
         setStoredValue(value);
         if (
+
+            // string number veya boolean ise direkt kaydet
             typeof value === "string" ||
             typeof value === "number" ||
             typeof value === "boolean"
         ) {
             localStorage.setItem(key, value);
         } else {
+            //değil ise (nesne veya obje ise) stringe dönüştür kaydet
             localStorage.setItem(key, JSON.stringify(value));
         }
     };
