2d5ce43072f93d31ed40f91ee03447c144626428
eceykilic <eceykilic@gmail.com>
2024-03-04 21:01:10 +0300

    T14: Add Product to shopping cart dropdown component created, reducers created #29

--- ecommerce/src/components/Layout/ShoppingCartDropdown.jsx
+++ ecommerce/src/components/Layout/ShoppingCartDropdown.jsx
@@ -4,16 +4,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const ShoppingCartDropdown = ({ onClose }) => {
-   const shoppingCart = useSelector((store) => store.shoppingCart);
+  const shoppingCart = useSelector((store) => store.shoppingCart);
   console.log("ShoppingCartDropdown - shoppingCart:", shoppingCart);
-
+  const cart = useSelector((store) => store.shoppingCart.cartList);
   const cartList = useSelector((store) => store.shoppingCart.cartList);
   console.log("ShoppingCartDropdown - cartList:", cartList);
 
   return (
-    <div className="absolute right-0 top-12 bg-white border border-gray-300 p-4 rounded-md shadow-lg z-50">
+    <div className="absolute top-12 p-5 right-1 bg-white border border-gray-300 rounded-md shadow-lg z-50 min-w-96 min-h-96">
       <div className="flex justify-between items-center mb-2">
-        <h3 className="text-lg font-semibold">Shopping Cart</h3>
+        <h3 className="text-lg font-semibold">Shopping Cart <span>({cart.length} Ürün)</span></h3>
         <FontAwesomeIcon
           icon={faTimes}
           className="cursor-pointer"
@@ -21,14 +21,34 @@ const ShoppingCartDropdown = ({ onClose }) => {
         />
       </div>
       {cartList.length > 0 ? (
-        <ul className="space-y-2">
+        <div className="">
           {cartList.map((item) => (
-            <li key={item.id} className="flex justify-between">
-              <span>{item.name}</span>
-              <span>{item.count}</span>
-            </li>
+            <div key={item.id} className="flex gap-8 border-b-2 py-2">
+              <img
+                className="w-24 h-32 border-2 rounded-lg object-cover my-2"
+                src={item.images[0].url}
+                alt=""
+              />
+              <div className="flex flex-col mr-10 gap-2 justify-center">
+              <p className="text-md font-semibold">{item.name}</p>
+              <p className="text-sm text-lighterDark font-medium">Size:M Count:{item.count}</p>
+              <p className="text-sm">${item.price}</p>
+              </div>
+            </div>
+            
           ))}
-        </ul>
+           <div className="flex gap-3 mt-4 justify-between">
+                    <button
+                      onClick={() => history.push("/cart")}
+                      className="py-2 px-5 rounded-md bg-gray-100 hover:bg-gray-700"
+                    >
+                      Go to Cart
+                    </button>
+                    <button className="py-2 px-3 rounded-md bg-navBlue text-white hover:bg-gray-700 hover:text-navBlue">
+                      Proceed to Checkout
+                    </button>
+                  </div>
+        </div>
       ) : (
         <p>Your shopping cart is empty.</p>
       )}
